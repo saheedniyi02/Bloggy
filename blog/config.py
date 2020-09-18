2db7d7773257444d89115c69f445ddb8618a2775
saheedniyi02 <64325621+saheedniyi02@users.noreply.github.com>
2020-09-18 19:24:40 +0100

    Add files via upload

---
+++ blog/config.py
@@ -0,0 +1,8 @@
+class Config:
+	SECRET_KEY="aragggtdsseryhbbbbbg2555aseghhkeywouldbegetee"
+	SQLALCHEMY_DATABASE_URI='sqlite:///site.db'
+	MAIL_SERVER="smtp.gmail.com"
+	MAIL_PORT=587
+	MAIL_USE_TLS=True
+	MAIL_USERNAME="flaskappmailsender@gmail.com"
+	MAIL_PASSWORD="MailPasswordHere"
